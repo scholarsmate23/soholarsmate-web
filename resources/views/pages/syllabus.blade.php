8125b2e03c61b905db6c6951557bac44db13dfd5
nationlearns <contact@nationlearns.com>
2024-02-25 20:06:10 +0530

    changes in download pdf

--- resources/views/pages/syllabus.blade.php
+++ resources/views/pages/syllabus.blade.php
@@ -39,7 +39,7 @@
 
                     <tr>
                     <td>NEET</td>
-                    <td><a href="{{ route('pdf.download', ['id' => 2]) }}">Click to Download</a></td>
+                    <td><a href="{{ route('pdf.download', ['id' => 3]) }}">Click to Download</a></td>
                     </tr>
 
 
@@ -58,8 +58,20 @@
                     </tr>
 
                     <tr>
-                    <td>Maths/BIO</td>
-                    <td><a href="">Click to Download</a></td>
+                    <td>Maths</td>
+                    <td><a href="{{ route('pdf.download', ['id' => 4]) }}">Click to Download</a></td>
+                    </tr>
+                    <tr>
+                    <td>Physics</td>
+                    <td><a href="{{ route('pdf.download', ['id' => 6]) }}">Click to Download</a></td>
+                    </tr>
+                    <tr>
+                    <td>Chemistry</td>
+                    <td><a href="{{ route('pdf.download', ['id' => 5]) }}">Click to Download</a></td>
+                    </tr>
+                    <tr>
+                    <td>Biology</td>
+                    <td><a href="{{ route('pdf.download', ['id' => 7]) }}">Click to Download</a></td>
                     </tr>
 
                 </table>
@@ -78,16 +90,16 @@
 
                     <tr>
                     <td>Class 10th</td>
-                    <td><a href="">Click to Download</a></td>
+                    <td><a href="{{ route('pdf.download', ['id' => 8]) }}">Click to Download</a></td>
                     </tr>
 
                     <tr>
                     <td>Class 9th</td>
-                    <td><a href="">Click to Download</a></td>
+                    <td><a href="{{ route('pdf.download', ['id' => 8]) }}">Click to Download</a></td>
                     </tr>
                     <tr>
                     <td>Class 8th</td>
-                    <td><a href="">Click to Download</a></td>
+                    <td><a href="{{ route('pdf.download', ['id' => 9]) }}">Click to Download</a></td>
                     </tr>
 
                 </table>
