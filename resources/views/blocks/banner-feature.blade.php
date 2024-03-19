5eb322651c2814787832dc24e489170d24ecd3e5
nationlearns <contact@nationlearns.com>
2024-03-19 09:56:48 +0530

    changes in events

--- resources/views/blocks/banner-feature.blade.php
+++ resources/views/blocks/banner-feature.blade.php
@@ -3,113 +3,111 @@
   <div class="container-fluid p-0">
     <div class="row no-gutters">
       <div class="col-xl-8 col-lg-7 ">
-      <h2 class="section-title" style="margin: 0 40px;">Latest News and Events</h2>
-      <div class="accordion accordion-flush" style="margin: 10px 60px;" id="accordionFlushExample">
-      <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingOne">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-      <strong>उद्दाम - Career Counselling</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
-        <li class="list-style">
-        Counselling will be provided to candidate Who have any Confusion about Course, Career, University or College.
-          <a style="color:blue;" data-toggle="openModalDetais" data-target="#uddyamDetailModal">Click to know More</a>
-        </li>
-      </div>
-    </div>
-  </div>
-    <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-        <strong>Scholarships</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
-        <li class="list-style">
-          Scholarships will be provided to candidate who meet the folloeing Criteria.
-          <a style="color:blue;" data-toggle="openModal" data-target="#scholarshipsDetailModal">Click to know More</a>
-        </li>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-      <strong>आकलन-2024</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
-        <li class="list-style">Aakalan is an aptitudes lest of Scholar's Matee Career Institute -Bhagalpur for encouragement and Assessment of the student's, it is a specially designed initiative to encourage young talent with cash prizes, Scholarships and monitoring by Scholar's Mate career institute - BHAGALPUR.
-        <a href="{{route('akalan')}}">Know More</a>
-        </li>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingFour">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-        <strong>TAD-2024 (ICSE/CBSE)</strong> 
-      </button>
-    </h2>
-    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
-        <ul>
-        <li class="list-style">TAD - CBSE(2024) <a href="{{route('tad.cbse')}}">Know More</a></li>
-        <li class="list-style">TAD - ICSE(2024) <a href="{{route('tad.icse')}}">Know More</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingFive">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
-      <strong>प्रयास - Test Series(JEE/NEET)</strong>
-      </button>
-    </h2>
-    <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
-        <ul>
-        <li class="list-style">JEE Test Series Planner 2023-24 <a href="{{route('prayash.jee')}}">Know More</a></li>
-        <li class="list-style">NEET Test Series Planner 2023-24 <a href="{{route('prayash.neet')}}">Know More</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="flush-headingSix">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
-       <strong>Admission Open for the Courses</strong> 
-      </button>
-    </h2>
-    <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">
-        <ul class="list-design">
-        <li class="list-style"> उद्घोष-JEE (Foundation Course) <a href="{{route('udgosh.jee')}}">Know More</a></li>
-        <li class="list-style">सफल -JEE (Target Course) <a href="{{route('safal.jee')}}">Know More</a></li>
-        <li class="list-style">उद्घोष-NEET (Foundation Course) <a href="{{route('udgosh.neet')}}">Know More</a></li>
-        <li class="list-style">सफल-NEET (Target Course) <a href="{{route('safal.neet')}}">Know More</a></li>
-        <li class="list-style">एकलव्य -Tenth <a href="{{route('home')}}">Know More</a></li>
-        <li class="list-style">एकलव्य - Ninth <a href="{{route('home')}}">Know More</a></li>
-        <li class="list-style">एकलव्य - Eighth <a href="{{route('home')}}">Know More</a></li>
-        <li class="list-style">तरूण - Maths <a href="{{route('home')}}">Know More</a></li>
-        <li class="list-style">तरूण - Biology <a href="{{route('home')}}">Know More</a></li>
+        <h2 class="section-title" style="margin: 0 40px;">Latest News and Events</h2>
+        <div class="accordion accordion-flush" style="margin: 10px 60px;" id="accordionFlushExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <strong>उद्दाम - Career Counselling</strong>
+              </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+                <li class="list-style">
+                  Counselling will be provided to candidate Who have any Confusion about Course, Career, University or College.
+                  <a style="color:blue;" data-toggle="openModalDetais" data-target="#uddyamDetailModal">Click to know More</a>
+                </li>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingTwo">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <strong>Scholarships</strong>
+              </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+                <li class="list-style">
+                  Scholarships will be provided to candidate who meet the folloeing Criteria.
+                  <a style="color:blue;" data-toggle="openModal" data-target="#scholarshipsDetailModal">Click to know More</a>
+                </li>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingThree">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                <strong>आकलन-2024</strong>
+              </button>
+            </h2>
+            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+                <li class="list-style">Aakalan is an aptitudes lest of Scholar's Matee Career Institute -Bhagalpur for encouragement and Assessment of the student's, it is a specially designed initiative to encourage young talent with cash prizes, Scholarships and monitoring by Scholar's Mate career institute - BHAGALPUR.
+                  <a href="{{route('akalan')}}">Know More</a>
+                </li>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingFour">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                <strong>TAD-2024 (ICSE/CBSE)</strong>
+              </button>
+            </h2>
+            <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+                <ul>
+                  <li class="list-style">TAD - CBSE(2024) <a href="{{route('tad.cbse')}}">Know More</a></li>
+                  <li class="list-style">TAD - ICSE(2024) <a href="{{route('tad.icse')}}">Know More</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingFive">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                <strong>प्रयास - Test Series(JEE/NEET)</strong>
+              </button>
+            </h2>
+            <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+                <ul>
+                  <li class="list-style">JEE Test Series Planner 2023-24 <a href="{{route('prayash.jee')}}">Know More</a></li>
+                  <li class="list-style">NEET Test Series Planner 2023-24 <a href="{{route('prayash.neet')}}">Know More</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingSix">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                <strong>Admission Open for the Courses</strong>
+              </button>
+            </h2>
+            <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">
+                <ul class="list-design">
+                  <li class="list-style"> उद्घोष-JEE (Foundation Course) <a href="{{route('udgosh.jee')}}">Know More</a></li>
+                  <li class="list-style">सफल -JEE (Target Course) <a href="{{route('safal.jee')}}">Know More</a></li>
+                  <li class="list-style">उद्घोष-NEET (Foundation Course) <a href="{{route('udgosh.neet')}}">Know More</a></li>
+                  <li class="list-style">सफल-NEET (Target Course) <a href="{{route('safal.neet')}}">Know More</a></li>
+                  <li class="list-style">एकलव्य -Tenth <a href="{{route('home')}}">Know More</a></li>
+                  <li class="list-style">एकलव्य - Ninth <a href="{{route('home')}}">Know More</a></li>
+                  <li class="list-style">एकलव्य - Eighth <a href="{{route('home')}}">Know More</a></li>
+                  <li class="list-style">तरूण - Maths <a href="{{route('home')}}">Know More</a></li>
+                  <li class="list-style">तरूण - Biology <a href="{{route('home')}}">Know More</a></li>
 
-        </ul>
+                </ul>
 
-      </div>
-    </div>
-  </div>
-</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="col-xl-4 col-lg-5">
-      <img class="img-fluid w-100" src="{{asset('assets/source/images/banner/abt.jpg')}}" alt="banner-feature">
-
+        <img class="img-fluid w-100" src="{{asset('assets/source/images/banner/abt.jpg')}}" alt="banner-feature">
       </div>
-    </div>
   </div>
 </section>
 <!-- /banner-feature -->
--- resources/views/events-page.blade.php
+++ resources/views/events-page.blade.php
@@ -1,5 +1,5 @@
-@include('header') 
-@include('blocks/navigation') 
+@include('header')
+@include('blocks/navigation')
 @include('blocks/page-title')
 
 <!-- courses -->
@@ -7,98 +7,68 @@
     <div class="container">
         <div class="row">
             <!-- event -->
-            <div class="col-lg-4 col-sm-6 mb-5">
-                <div class="card border-0 rounded-0 hover-shadow">
-                    <div class="card-img position-relative">
-                        <img class="card-img-top rounded-0" src="{{asset('assets/source/images/events/event-1.jpg')}}" alt="event thumb">
-                        <div class="card-date"><span>18</span><br>December</div>
-                    </div>
-                    <div class="card-body">
-                        <!-- location -->
-                        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi Lake, Dhaka</p>
-                        <a href="event-single.html">
-                            <h4 class="card-title">Lorem ipsum dolor amet, consectetur adipisicing.</h4>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- event -->
-            <div class="col-lg-4 col-sm-6 mb-5">
-                <div class="card border-0 rounded-0 hover-shadow">
-                    <div class="card-img position-relative">
-                        <img class="card-img-top rounded-0" src="{{asset('assets/source/images/events/event-2.jpg')}}" alt="event thumb">
-                        <div class="card-date"><span>21</span><br>December</div>
-                    </div>
-                    <div class="card-body">
-                        <!-- location -->
-                        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi Lake, Dhaka</p>
-                        <a href="event-single.html">
-                            <h4 class="card-title">Lorem ipsum dolor amet, consectetur adipisicing.</h4>
-                        </a>
+            <div class="row justify-content-center">
+                <!-- event -->
+                <div class="col-lg-3 col-sm-6 mb-5 mb-lg-0">
+                    <div class="card border-0 rounded-0 hover-shadow">
+                        <!-- <div class="card-img position-relative">
+        <div class="card-date"><span>1</span><br>April</div>
+      </div> -->
+                        <div class="card-body">
+                            <!-- location -->
+                            <a href="{{route('udgosh.jee')}}">
+                                <h4 class="card-title">उद्घोष-JEE</h4>
+                            </a>
+                            <p>Commencement On: <strong>1 April 2024</strong></p>
+                            <p><i class="ti-location-pin text-primary mr-2"></i>Bhagalpur</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- event -->
-            <div class="col-lg-4 col-sm-6 mb-5">
-                <div class="card border-0 rounded-0 hover-shadow">
-                    <div class="card-img position-relative">
-                        <img class="card-img-top rounded-0" src="{{asset('assets/source/images/events/event-3.jpg')}}" alt="event thumb">
-                        <div class="card-date"><span>23</span><br>December</div>
-                    </div>
-                    <div class="card-body">
-                        <!-- location -->
-                        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi Lake, Dhaka</p>
-                        <a href="event-single.html">
-                            <h4 class="card-title">Lorem ipsum dolor amet, consectetur adipisicing.</h4>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- event -->
-            <div class="col-lg-4 col-sm-6 mb-5">
-                <div class="card border-0 rounded-0 hover-shadow">
-                    <div class="card-img position-relative">
-                        <img class="card-img-top rounded-0" src="{{asset('assets/source/images/events/event-1.jpg')}}" alt="event thumb">
-                        <div class="card-date"><span>18</span><br>December</div>
-                    </div>
-                    <div class="card-body">
-                        <!-- location -->
-                        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi Lake, Dhaka</p>
-                        <a href="event-single.html">
-                            <h4 class="card-title">Lorem ipsum dolor amet, consectetur adipisicing.</h4>
-                        </a>
+                <!-- event -->
+                <div class="col-lg-3 col-sm-6 mb-5 mb-lg-0">
+                    <div class="card border-0 rounded-0 hover-shadow">
+                        <!-- <div class="card-img position-relative">
+        <div class="card-date"><span>1</span><br>April</div>
+      </div> -->
+                        <div class="card-body">
+                            <!-- location -->
+                            <a href="{{route('udgosh.neet')}}">
+                                <h4 class="card-title">उद्घोष-NEET</h4>
+                            </a>
+                            <p>Commencement On: <strong>1 April 2024</strong></p>
+                            <p><i class="ti-location-pin text-primary mr-2"></i>Bhagalpur</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- event -->
-            <div class="col-lg-4 col-sm-6 mb-5">
-                <div class="card border-0 rounded-0 hover-shadow">
-                    <div class="card-img position-relative">
-                        <img class="card-img-top rounded-0" src="{{asset('assets/source/images/events/event-2.jpg')}}" alt="event thumb">
-                        <div class="card-date"><span>21</span><br>December</div>
-                    </div>
-                    <div class="card-body">
-                        <!-- location -->
-                        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi Lake, Dhaka</p>
-                        <a href="event-single.html">
-                            <h4 class="card-title">Lorem ipsum dolor amet, consectetur adipisicing.</h4>
-                        </a>
+                <!-- event -->
+                <div class="col-lg-3 col-sm-6 mb-5 mb-lg-0">
+                    <div class="card border-0 rounded-0 hover-shadow">
+                        <!-- <div class="card-img position-relative">
+        <div class="card-date"><span>15</span><br>April</div>
+      </div> -->
+                        <div class="card-body">
+                            <!-- location -->
+                            <a href="{{route('safal.jee')}}">
+                                <h4 class="card-title">सफल -JEE</h4>
+                            </a>
+                            <p>Commencement On: <strong>15 April 2024</strong></p>
+                            <p><i class="ti-location-pin text-primary mr-2"></i>Bhagalpur</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- event -->
-            <div class="col-lg-4 col-sm-6 mb-5">
-                <div class="card border-0 rounded-0 hover-shadow">
-                    <div class="card-img position-relative">
-                        <img class="card-img-top rounded-0" src="{{asset('assets/source/images/events/event-3.jpg')}}" alt="event thumb">
-                        <div class="card-date"><span>23</span><br>December</div>
-                    </div>
-                    <div class="card-body">
-                        <!-- location -->
-                        <p><i class="ti-location-pin text-primary mr-2"></i>Dhanmondi Lake, Dhaka</p>
-                        <a href="event-single.html">
-                            <h4 class="card-title">Lorem ipsum dolor amet, consectetur adipisicing.</h4>
-                        </a>
+                <div class="col-lg-3 col-sm-6 mb-5 mb-lg-0">
+                    <div class="card border-0 rounded-0 hover-shadow">
+                        <!-- <div class="card-img position-relative">
+        <div class="card-date"><span>15</span><br>April</div>
+      </div> -->
+                        <div class="card-body">
+                            <!-- location -->
+                            <a href="{{route('safal.neet')}}">
+                                <h4 class="card-title">सफल -NEET</h4>
+                            </a>
+                            <p>Commencement On: <strong>15 April 2024</strong></p>
+                            <p><i class="ti-location-pin text-primary mr-2"></i>Bhagalpur</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -107,5 +77,5 @@
 </section>
 <!-- /courses -->
 
-@include('blocks/footer') 
+@include('blocks/footer')
 @include('footer')
